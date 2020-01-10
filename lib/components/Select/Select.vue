d6eb644313db24d755cee5b7860f2d3c6a398a8f
Gergely Szabo <gergely0116@gmail.com>
2020-01-10 17:15:51 +0100

    adding skeleton select

---
+++ lib/components/Select/Select.vue
@@ -0,0 +1,41 @@
+<template>
+	<component
+		:is="renderableComponent"
+
+		:options="options"
+		:required="required"
+	/>
+</template>
+
+<script>
+import ContainerComponent from '../ContainerComponent'
+
+const Loader = require('@dwmt/loader/lib/Loader')
+
+export default {
+	name: 'Select',
+	extends: ContainerComponent,
+	props: {
+		options: {
+			required: true
+		},
+		required: {
+			default: false
+		},
+		placeholder: {},
+		value: {},
+		label: {}
+	},
+	methods: {
+		click () {},
+		input () {},
+		focus () {},
+		blur () {},
+		keyup () {}
+	}
+}
+</script>
+
+<style>
+
+</style>
