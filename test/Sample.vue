c5063a497fd11cc1a6107fbdcfc4e4945984bb63
Kovács Lajos <kovacs.lajos1218@gmail.com>
2020-01-10 12:36:18 +0100

    Added test suite

---
+++ test/Sample.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<span class="sample">ASD</span>
+	</div>
+</template>
+<script>
+export default {
+	name: 'Sample'
+}
+</script>
+<style>
+.sample {
+	color: red;
+}
+</style>
