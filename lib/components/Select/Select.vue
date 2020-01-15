3bd7c2ddbb2668551bef865f7a0202d7e728e58d
Kovács Lajos <kovacs.lajos1218@gmail.com>
2020-01-15 13:39:48 +0100

    Working select input

--- lib/components/Select/Select.vue
+++ lib/components/Select/Select.vue
@@ -2,53 +2,155 @@
 	<component
 		:is="renderableComponent"
 
-		:options="options"
+		:options="selectionOptions"
 		:required="required"
-		:placeholder="placeholder"
-		:value="value"
+		:value="selectionValue"
 		:label="label"
+		:toggled="toggled"
 
 		@click="click"
-		@input="input"
 		@focus="focus"
 		@blur="blur"
 		@keyup="keyup"
+		@input="select"
+		@toggle="toggle"
 	/>
 </template>
 
 <script>
 import ContainerComponent from '../ContainerComponent'
+import {optionalChaining} from '../../utils'
 
 const Loader = require('@dwmt/loader/lib/Loader')
+
+const OPTIONS_TYPE_ARRAY = 'OPTIONS_TYPE_ARRAY'
+const OPTIONS_TYPE_KEY_VALUE_NORMAL = 'OPTIONS_TYPE_KEY_VALUE_NORMAL'
+const OPTIONS_TYPE_KEY_VALUE_LOCALIZED = 'OPTIONS_TYPE_KEY_VALUE_LOCALIZED'
+
+const OPTIONS_TYPES = {
+	'ARRAY': OPTIONS_TYPE_ARRAY,
+	'NORMAL': OPTIONS_TYPE_KEY_VALUE_NORMAL,
+	'LOCALIZED': OPTIONS_TYPE_KEY_VALUE_LOCALIZED
+}
 
 export default {
 	name: 'Select',
 	extends: ContainerComponent,
 	props: {
 		options: {
-			required: true
+			type: Array,
+			default: () => []
 		},
 		required: {
 			default: false
 		},
-		placeholder: {},
 		value: {},
-		label: {}
+		label: {},
+		localisation: {
+			type: Boolean,
+			default: false
+		}
 	},
 	data () {
 		return {
 			component: 'Select',
 			defaultComponent: 'default-select',
 			loader: null,
-			isLoading: false
+			isLoading: false,
+			toggled: false
+		}
+	},
+	computed: {
+		selectionValue () {
+			let optionsType = this.optionsType
+			let value = this.selectionOptions.find((option) => {
+				return option.value === this.value
+			})
+			if (!value)
+				return undefined
+			return value.key
+		},
+		optionsType () {
+			let allString = true
+			let allObject = true
+			let allNormalKey = true
+			let allObjKey = true
+			this.options.forEach(option => {
+				if (typeof option === 'string') {
+					allObject = false
+				}
+				if (typeof option === 'object') {
+					allString = false
+					if (typeof option.key === 'string') {
+						allObjKey = false
+					}
+					if (typeof option.key === 'object') {
+						allNormalKey = false
+					}
+				}
+			})
+			if (allString) {
+				return OPTIONS_TYPES.ARRAY
+			}
+
+			if (allObject && allNormalKey) {
+				return OPTIONS_TYPES.NORMAL
+			}
+
+			if (allObject && allObjKey) {
+				return OPTIONS_TYPES.LOCALIZED
+			}
+			throw new Error('Provided options array is invalid!')
+		},
+		selectionOptions () {
+			if (!this.options || !this.options.length) {
+				return []
+			}
+			let optionsType = this.optionsType
+			
+			if (optionsType === OPTIONS_TYPES.ARRAY) {
+				return this.options.map((o) => { return {key: o, value: o} })
+			}
+
+			if (optionsType === OPTIONS_TYPES.NORMAL && !this.localisation) {
+				return this.options
+			}
+			if (optionsType === OPTIONS_TYPES.NORMAL && this.localisation) {
+				return this.options.map((o) => {
+					return {
+						key: this.translate(o.key),
+						value: o.value
+					}
+				})
+			}
+
+			if (optionsType === OPTIONS_TYPES.LOCALIZED) {
+				return this.options.map((o, key) => {
+					return {
+						key: o[optionalChaining(this, $i18n.locale)] || o.default || 'No Key #' + key,
+						value: o.value
+					}
+				})
+			}
+
 		}
 	},
 	methods: {
+		toggle () {
+			this.toggled = !this.toggled
+		},
+		translate (key) {
+			if (!this.$t) {
+				return key
+			}
+			return this.$t(key)
+		},
 		click (payload) {
 			this.$emit('click', payload)
 		},
-		input (payload) {
-			this.$emit('input', payload)
+		select (key) {
+			this.$emit('input', this.selectionOptions[key].value)
+			this.toggled = false
 		},
 		focus (payload) {
 			this.$emit('focus', payload)
@@ -72,7 +174,3 @@ export default {
 	}
 }
 </script>
-
-<style>
-
-</style>
