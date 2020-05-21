731fecb347b6bd7a49d97ec078d8ea78ac4dc544
Kovács Lajos <kovacs.lajos1218@gmail.com>
2020-05-21 16:29:40 +0200

    [0.4.1] Fixed Sorter component

---
+++ src/components/Sorter/DefaultSorter.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+.asd
+</template>
+
+<script>
+import ElementForgeTheme from '@dwmt/elementforge-theme'
+
+export default {
+	name: 'DefaultSorter',
+	extends: ElementForgeTheme.RenderableComponent,
+	props: ElementForgeTheme.props.Sorter.renderable,
+}
+</script>
--- src/components/Sorter/Sorter.vue
+++ src/components/Sorter/Sorter.vue
@@ -1,5 +1,5 @@
-<template>
-	<component
+<template lang="pug">
+	component(
 		:is="renderableComponent"
 
 		:darkMode="darkMode"
@@ -12,12 +12,18 @@
 		:disabled="disabled"
 
 		@input="input"
-	/>
+	)
 </template>
 
 <script>
 import ElementForgeTheme from '@dwmt/elementforge-theme'
 import ContainerComponent from '../ContainerComponent.vue'
+
+let stateMatrix = {
+	'0': 1,
+	'1': -1,
+	'-1': 0
+}
 
 export default {
 	name: 'Sorter',
@@ -30,53 +36,13 @@ export default {
 		}
 	},
 	computed: {
-		checked () {
-			return equal(this.valueComputed, this.dataComputed)
-		},
-		valueComputed () {
-			if (this.radioGroup) {
-				return this.valueInherit
-			}
-			return this.value
-		},
-		disabledComputed () {
-			let disabledInherit = (this.disabledInherit === null) ? false : this.disabledInherit
-
-			return this.disabledInherit || this.disabled
-		},
-		groupIdentifier () {
-			return `${this.nameComputed}-${this.dataComputed}`
+		nextState () {
+			return stateMatrix[this.value.toString()]
 		}
 	},
 	methods: {
-		click (payload) {
-			this.$emit('click', payload)
-		},
 		input () {
-			this.$emit('input', this.dataComputed)
-			if (this.radioGroup) {
-				this.radioGroup.input(this.dataComputed)
-			}
-		}
-	},
-	mounted () {
-		if (typeof this.data === 'undefined') {
-			throw new Error('Data property is mandatory!')
-		}
-		this.dataComputed = this.data
-		this.nameComputed = this.name || 'radio-' + Math.floor(Math.random() * 100) + 1
-		if (this.radioGroup) {
-			let inheritValues = this.radioGroup.registerEntry(this.groupIdentifier, (newValue) => {
-				this.valueInherit = newValue.value
-				this.disabledInherit = newValue.disabled
-			})
-			this.valueInherit = inheritValues.value
-			this.disabledInherit = inheritValues.disabled
-		}
-	},
-	beforeDestroy () {
-		if (this.radioGroup) {
-			this.radioGroup.deleteEntry(this.groupIdentifier)
+			this.$emit('input', this.nextState)
 		}
 	}
 }
