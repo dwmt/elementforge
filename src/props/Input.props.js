79446bd48ba0af2d27b325363e45c0b6af1a8e8b
Kovács Lajos <lajoskvcs@icloud.com>
2021-03-04 15:45:56 +0100

    [2.0.0-rc8] Fixes

--- src/props/Input.props.js
+++ src/props/Input.props.js
@@ -1,44 +1,47 @@
 const props = {
-  label: {
-    type: String,
-    default: ''
-  },
-  type: {},
-  modelValue: {},
-  editable: {
-    type: Boolean,
-    default: true
-  },
-  required: {
-    default: false,
-    type: Boolean
-  },
-  disabled: {
-    default: false,
-    type: Boolean
-  },
-  precision: {
-    type: Number,
-    default: 0
-  },
-  max: {
-    type: Number
-  },
-  min: {
-    type: Number
-  }
+	label: {
+		type: String,
+		default: ''
+	},
+	type: {},
+	modelValue: {},
+	editable: {
+		type: Boolean,
+		default: true
+	},
+	required: {
+		default: false,
+		type: Boolean
+	},
+	disabled: {
+		default: false,
+		type: Boolean
+	},
+	precision: {
+		type: Number,
+		default: 0
+	},
+	max: {
+		type: Number
+	},
+	min: {
+		type: Number
+	}
 }
 
 const container = {
-  ...props
+	...props
 }
 
 const renderable = {
-  ...props,
-  editable: {}
+	...props,
+	editable: {
+		type: Boolean,
+		default: true
+	}
 }
 
 export default {
-  container,
-  renderable
+	container,
+	renderable
 }
